fcc17a2de2b1fc951a4d330010e2c808bdec124c
myos84 <a.thery1704@gmail.com>
2022-01-17 22:58:04 +0100

    MAJ lien pendu

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+function abracadabra() {
+    let prenom = prompt('Quel est votre prénom ?');
+    let nom    = prompt('Quel est votre nom ?');
+    let age    = prompt('quel est votre Age ?');
+
+    alert('Sapristi ! on ne m\'avait pas prévenu que c\'était vous ' + prenom + ' ! Euh... Je veux dire... Monsieur le grand magicien ' + nom + ' ! Cela fait déja ' + age + ' ans que vous faites rayonner notre contrée !' );
+}
+
+abracadabra();
