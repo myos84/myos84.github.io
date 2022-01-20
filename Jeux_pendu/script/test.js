ab2b0b634d3f0153fa26d431756e550f0f974651
myos84 <a.thery1704@gmail.com>
2022-01-20 22:45:54 +0100

    ajout de la fonction mot aleatoire , decomposition du mot ainsi que la recherhce de caractere dans le mot

--- Jeux_pendu/script/test.js
+++ Jeux_pendu/script/test.js
@@ -1,32 +1,56 @@
-let monTableau = ['voiture', 'believemy', 'formation', 'ratatouille'];
-let motAleatoir = Math.floor(Math.random()*monTableau.length);
-let decompose = monTableau[motAleatoir].split('');
-let lettre;
-let test = document.createElement('div');
-let titre = document.querySelector('.mot');
-console.log(decompose);
+let tableauDeMot = ["VOITURE", "AVION", "ELEPHANT", "FORMATION", "ECOLE", "ENDIVE"];
+let motDecompose;
+let tableauLettre = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lettreChoisie
 
-titre.innerHTML = '<h1> Le mot est : ' + decompose + ' </h1>'
+// Réalisation du Clavier Virtuel avec recuperation du click sur les touches 
+
+            function clavierVirtuel() {
+                    let table = document.createElement('table');
+                    table.className = 'table';
+                    let tbody = document.createElement('tbody');
+                    let tr = document.createElement('tr');
+                    let alphabetLetters = document.querySelector('.alphabetLetters')
+
+                            for (const letters of tableauLettre) {
+                                let td = document.createElement('td');
+                                td.className = 'tableTd';
+                                td.innerText = letters;
+                                tr.appendChild(td);
+                                td.addEventListener('click', () => {
+                                console.log(letters);
+                            })
+                            }
+                
+                tbody.appendChild(tr);
+                table.appendChild(tbody);
+                alphabetLetters.appendChild(table);
+            
+            }
+                console.log(clavierVirtuel());
+
+// Fonction permettant de recuperer un mot aleatoire dans le tableau et de le decomposer 
+
+            function elemAleatoire() { 
+                    let motAleatoire = tableauDeMot[Math.floor(Math.random()*tableauDeMot.length)];
+                    motDecompose = motAleatoire.split('');
+                    return motDecompose;
+            } 
+
+                console.log(elemAleatoire());
 
 
+// Fonction permettant verifier les caracteres et de les afficher si present ou retirer 1 coup si absent 
 
+            function motCache() {
+            
+                        for(var i=0; i<motDecompose.length; i++) {
+                            let el = "";
+                            if(el === motDecompose[i]){ 
+                            console.log('element present')
+                            } else {
+                            }
+                        }
+            }
 
-const controle = () =>  {
-
-lettre = document.addEventListener('keypress', (e) =>  {
-    lettre = event.key;
-
-
-for(var i=0; i<(decompose.length); i++){
-    if(lettre == decompose[i]) {
-        test = 'la lettre ' + lettre + ' est à l\'index ' + decompose.indexOf(lettre) + '<br>' ;
-        document.body.innerHTML += '<strong> ' + test + ' </strong> <br>';
-    }
-}
-}) 
-}
-
-
-
-controle()
-
+            motCache()
